5c47f0b20297ff7003a5a1f8058296966fd84f07
Raturu0 <aziz.fauzi1811@gmail.com>
2023-07-18 12:13:43 +0700

    First commit TO DO LIST

---
+++ script.js
@@ -0,0 +1,21 @@
+// Ambil button
+const add = document.querySelector(".add");
+
+// Ambil ul
+const ul = document.querySelector("ul");
+
+let i = 0;
+add.addEventListener("click", function() {
+    const inputan = document.querySelector(".inputan");
+    console.log(inputan.value);
+
+    const li = document.createElement("li");
+    // isi text
+    const text_li = document.createTextNode(`${i+1}. ${inputan.value}`);
+    i++;
+    // masukkan text ke li
+    li.appendChild(text_li);
+    // masukkan li ke ul
+    
+    ul.appendChild(li);
+})
