6ca9363708a2519ca9a7b305bc5ba66191ed30d7
Raturu0 <aziz.fauzi1811@gmail.com>
2023-07-18 17:30:08 +0700

    Menambahkan fitur coret daftar

--- script.js
+++ script.js
@@ -5,25 +5,34 @@ const add = document.querySelector(".add");
 const ul = document.querySelector("ul");
 
 let i = 0;
-add.addEventListener("click", function() {
-    const inputan = document.querySelector(".inputan");
-    console.log(inputan.value);
+add.addEventListener("click", function () {
+  const inputan = document.querySelector(".inputan");
+  console.log(inputan.value);
 
-    const li = document.createElement("li");
-    // isi text
-    const text_li = document.createTextNode(`${i+1}. ${inputan.value}`);
-    i++;
+  // Ambil Li
+  const li = document.createElement("li");
+  // Isikan text ke li
+  const text_li = document.createTextNode(`${i + 1}. ${inputan.value}`);
+  i++;
 
-    // Jika inputan kosong
-    if(inputan.value == "") {
-        alert("gagal menambahkan, silahkan mengisi inputan");
-        i--;
-        exit;
+  // Jika inputan kosong
+  if (inputan.value == "") {
+    alert("gagal menambahkan, silahkan mengisi inputan");
+    i--;
+    exit;
+  }
+  // masukkan text ke li
+  li.appendChild(text_li);
+  // masukkan li ke ul
+  ul.appendChild(li);
+  inputan.value = "";
+});
+
+
+
+ul.addEventListener("click", function(e) {
+    const li = e.target.closest("li");
+    if(li) {
+        li.classList.toggle("garisTengah");
     }
-
-    // masukkan text ke li
-    li.appendChild(text_li);
-    // masukkan li ke ul
-    ul.appendChild(li);
-    inputan.value = "";
-})
+});
