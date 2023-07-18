19d28709500150d0e8e437b15f47e45ba9abf94c
Raturu0 <aziz.fauzi1811@gmail.com>
2023-07-18 17:00:45 +0700

    menambahkan fitur kesalahan jika inputan kosong

--- script.js
+++ script.js
@@ -13,9 +13,17 @@ add.addEventListener("click", function() {
     // isi text
     const text_li = document.createTextNode(`${i+1}. ${inputan.value}`);
     i++;
+
+    // Jika inputan kosong
+    if(inputan.value == "") {
+        alert("gagal menambahkan, silahkan mengisi inputan");
+        i--;
+        exit;
+    }
+
     // masukkan text ke li
     li.appendChild(text_li);
     // masukkan li ke ul
-    
     ul.appendChild(li);
+    inputan.value = "";
 })
